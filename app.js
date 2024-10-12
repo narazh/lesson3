5ffafa23b2dcd0cbacb83327dac6b1ff068add06
Janara <zhanaraisirailova@gmail.com>
2024-10-12 09:42:08 +0600

    first commit

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/narazh/lesson3.git
-git push -u origin main
-
+git push -u origin master
 ctrl + j
 
 */
